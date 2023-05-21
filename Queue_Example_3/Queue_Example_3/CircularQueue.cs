676802967ccbd6c99cd18040bffaa0204ec67a62
Hosein Mohamadpor <83702864+younghosein@users.noreply.github.com>
2023-05-22 01:15:39 +0330

    Add files via upload

---
+++ Queue_Example_3/Queue_Example_3/CircularQueue.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_Example_3
+{
+    class CircularQueue
+    {
+        public int[] q;
+        public int front;
+        public int rear;
+        public int max;
+
+        public CircularQueue(int size)
+        {
+            q = new int[size];
+            max = size;
+            rear = -1;
+            front = -1;
+        }
+        public void print()
+        {
+            if (front == -1)
+            {
+                Console.WriteLine("Circular Queue Is Empty !");
+                return;
+            }
+            if (rear >= front)
+            {
+                for (int i = front; i <= rear; i++)
+                    Console.Write($"{q[i]} . ");
+            }
+            else
+            {
+                for (int i = front; i < max; i++)
+                    Console.Write($"{q[i]} . ");
+
+                for (int i = 0; i <= rear; i++)
+                    Console.Write($"{q[i]} . ");
+            }
+        }
+        public void enqueue(int data)
+        {
+            if ((front == 0 && rear == max - 1) || (rear == (front - 1) % (max - 1)))
+            {
+                Console.Write("Circular Queue Is Full !");
+                return;
+            }
+
+            else if (front == -1)
+            {
+                front = 0;
+                rear = 0;
+                q[rear] = data;
+            }
+
+            else if (rear == max - 1 && front != 0)
+            {
+                rear = 0;
+                q[rear] = data;
+            }
+
+            else
+            {
+                q[++rear] = data;
+            }
+        }
+        public int dequeue()
+        {
+            if (front == -1)
+            {
+                Console.WriteLine("Circular Queue Is Empty !");
+                return -1;
+            }
+
+            int data = q[front];
+            Console.WriteLine($"{q[front]} Is Deleted...");
+            q[front] = -1;
+            if (front == rear)
+            {
+                front = -1;
+                rear = -1;
+            }
+            else if (front == max - 1)
+                front = 0;
+            else
+                front++;
+
+            return data;
+        }
+        public int Avg()
+        {
+            int k;
+            int l = front;
+            int sum = 0;
+            int Avg;
+            int c = 0;
+            for (k = front; l < max - 1;)
+            {
+                sum += q[k];
+                c++;
+                k = (k + 1) % max;
+                l++;
+            }
+            Console.Write($"Sum Is : {sum}");
+            Console.Write($"\nCount Is : {c}\n");
+            Avg = sum / c;
+            return Avg;
+        }
+        public int Avgminus()
+        {
+            int k;
+            int l = front;
+            int sum = 0;
+            int Avg;
+            int c = 0;
+            Console.WriteLine("Minus Items Are : ");
+            for (k = front; l < max - 1;)
+            {
+                if (q[k] < 0)
+                {
+                    sum += q[k];
+                    Console.WriteLine(q[k]);
+                    c++;
+                }
+                k = (k + 1) % max;
+                l++;
+            }
+            Avg = sum / c;
+            return Avg;
+        }
+    }
+}
---
+++ Queue_Example_3/Queue_Example_3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_Example_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CircularQueue s = new CircularQueue(10);
+
+            s.enqueue(-20);
+            s.enqueue(69);
+            s.enqueue(-14);
+            s.enqueue(-93);
+            s.enqueue(57);
+            s.enqueue(19);
+            s.enqueue(-65);
+            s.enqueue(83);
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Circular Queue Is : ");
+            s.print();
+            Console.ResetColor();
+            Console.WriteLine("\n");
+
+            //Console.ForegroundColor = ConsoleColor.DarkRed;
+            //Console.WriteLine("Circular Queue Is : ");
+            //s.dequeue();
+            //s.dequeue();
+            //s.print();
+            //Console.ResetColor();
+            //Console.WriteLine("\n");
+
+            //Console.ResetColor();
+            //Console.ForegroundColor = ConsoleColor.Magenta;
+            //int a = s.Avg();
+            //Console.WriteLine("Average Is : ");
+            //Console.WriteLine(a);
+            //Console.ResetColor();
+
+            //Console.ResetColor();
+            //Console.ForegroundColor = ConsoleColor.Blue;
+            //int am = s.Avgminus();
+            //Console.WriteLine("Minus Average Is : ");
+            //Console.WriteLine(am);
+            //Console.ResetColor();
+
+            Console.ReadKey();
+        }
+    }
+}
